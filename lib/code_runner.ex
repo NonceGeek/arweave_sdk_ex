6ec7664550344322c25e01360086324f1af8b80d
leeduckgo <albertschr@163.com>
2021-11-21 23:28:29 +0800

    feat/add if_record in code-runner

--- lib/code_runner.ex
+++ lib/code_runner.ex
@@ -3,8 +3,16 @@ defmodule ArweaveSdkEx.CodeRunner do
     Run codes storage in arweave TX.
   """
   def get_ex_by_tx_id(node, tx_id) do
-    {:ok, %{decoded_tags: %{"Content-Type": type}}} =ArweaveSdkEx.get_tx(node, tx_id)
-    do_get_ex_by_tx_id(node, tx_id, type)
+    {:ok, %{decoded_tags: decoded_tags}} =ArweaveSdkEx.get_tx(node, tx_id)
+    type = Map.get(decoded_tags, :"Content-Type")
+
+    if_record = has_if_record(decoded_tags)
+    case do_get_ex_by_tx_id(node, tx_id, type) do
+      {:ok, content} ->
+        {:ok, %{code: content, if_record: if_record}}
+      {:error, msg} ->
+        {:error, msg}
+    end
   end
 
   defp do_get_ex_by_tx_id(node, tx_hash, "application/elixir") do
@@ -14,6 +22,15 @@ defmodule ArweaveSdkEx.CodeRunner do
 
   defp do_get_ex_by_tx_id(_node, _tx_id, _other_type) do
     {:error, "it's not a elixir func"}
+  end
+
+  def has_if_record(decoded_tags) do
+    raw_res = Map.get(decoded_tags, "If-Record")
+    if is_nil(raw_res) or raw_res=="0" do
+      false
+    else
+      true
+    end
   end
 
   # +--------+
