1fdcd3c5eab1ddea4f798890f69d44be0d439133
Ken Chen <chengusky@gmail.com>
2022-02-12 17:42:31 +0800

    impl get_root_hash from python to elixir

--- test/arweave_sdk_ex_test.exs
+++ test/arweave_sdk_ex_test.exs
@@ -2,7 +2,12 @@ defmodule ArweaveSdkExTest do
   use ExUnit.Case
   doctest ArweaveSdkEx
 
-  test "greets the world" do
-    assert ArweaveSdkEx.hello() == :world
+  test "gen_root_hash" do
+    data = File.read!(Path.join([__DIR__, "fixtures/testfile0.bin"]))
+
+    root_hash = ArweaveSdkEx.Tx.get_root_hash(data)
+
+    assert ArweaveSdkEx.Utils.Crypto.url_encode64(root_hash) ==
+             "YPg3Q4bkY2--cf4Ydjf5XnT1LXDKAVXVCDUaqkJ0Jvk"
   end
 end
